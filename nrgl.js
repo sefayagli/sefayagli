483f72d495d3e3442f160dd63126ec646781920b
sefayagli <129521515+sefayagli@users.noreply.github.com>
2024-06-22 13:37:55 +0300

    Add files via upload

--- nrgl.js
+++ nrgl.js
@@ -1,21 +1,23 @@
 const messages = [
     "En değerli hazinem ol benim.",
-    "Her anımda yanımda olmanı istiyorum.",
-    "Sana olan sevgim her geçen gün daha da artsın istiyorum.",
+    "Her anımda yanımda olmanı çok istiyorum.",
+    "Sana olan sevgim ve hayretim her geçen gün daha da artıyor.",
     "Gözlerinde hayatımın anlamını bulmak istiyorum.",
     "Seninle olmak dünyanın en güzel şeyi.",
     "Sana her baktığımda, seninle her konuştuğumda kalbim hızla çarpıyor.",
     "Seninle geçirdiğim her saniye bir ömür gibi.",
     "Sevgimiz sonsuza dek sürsün istiyorum.",
-    "Seni sevmek benim en büyük mutluluğum.",
-    "Her şeyim olmanı istiyorum."
+    "Seni sevmek,seni mutlu etmek benim en büyük mutluluğum.",
+    "Her şeyim olmanı istiyorum.",
+    "Birtanem..."
 ];
 
 const proposals = [
-    "Seninle daha fazla vakit geçirmek istiyorum, benimle çıkar mısın?",
-    "Hayatımın her anında seninle olmayı hayal ediyorum. Benimle çıkar mısın?",
-    "Kalbim hep seninle atıyor. Benimle çıkar mısın?",
-    "Sana olan duygularım tarifsiz. Benimle bir ilişkiye başlamayı düşünür müsün?",
+    "Kalan ömrümü seninle geçirmek istiyorum, evlenene kadar benimle çıkar mısın?",
+    "Evlilik yolunda ilerlemek üzere, benimle çıkar mısın?",
+    "Hayatımın her anında seninle olmayı çok isterim. Benimle çıkar mısın?",
+    "Kalbim hep seninle atıyor, atacak da. Benimle çıkar mısın?",
+    "Kısa bir sürede senden çok etkilendim. Benimle evlilik yolunda ilk adımı atar mısın?",
     "Seninle bir ömür geçirmek istiyorum. Benimle çıkar mısın?"
 ];
 
@@ -45,4 +47,24 @@ window.onload = function() {
     const content = document.getElementById('content');
     loader.style.display = 'none';
     content.style.display = 'block';
+}
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
